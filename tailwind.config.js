3834e3db770e3074621e18c7c3dacb5191543ede
“venens” <[patriks.venens@gmail.com]>
2025-04-15 15:26:20 +0300

    Created selected restaurant page layout and design with image gallery functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,17 +19,22 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+            },
+            gridTemplateRows: {
+                '[auto,auto,1fr]': 'auto auto 1fr',
+            },
         },
     },
 
     darkMode: 'class',
-    theme: {
-      extend: {
-        colors: {
-          primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
-        }
-      }
-    },
 
     plugins: [forms, require('flowbite')],
+
+
+    
+
+    // tailwind.config.js
+    // tailwind.config.js
 };
