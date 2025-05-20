c7a3df24c4422c79add23b6af3379b07e2976f49
“venens” <[patriks.venens@gmail.com]>
2025-05-20 15:19:39 +0300

    fixed restaurant info not being saved

--- resources/js/Pages/RestaurantOwner/Restaurant/RestaurantInfo.vue
+++ resources/js/Pages/RestaurantOwner/Restaurant/RestaurantInfo.vue
@@ -176,8 +176,8 @@ const saveChanges = async () => {
     <div v-if="restaurant" class="mx-auto max-w-screen-xl px-4 lg:px-12">
         <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
             <div class="overflow-x-auto pt-10 pb-10">
-                            <!-- form start -->
-            <form @submit.prevent="editMode ? updateRestaurant():AddRestaurant()" class="max-w-md mx-auto">
+            <!-- form start -->
+            <form @submit.prevent="updateRestaurant()" class="max-w-md mx-auto">
             <div class="relative z-0 w-full mb-5 group">
                 <input v-model="title" type="text" name="floating_title" id="floating_title" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                 <label for="floating_title" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Title</label>
